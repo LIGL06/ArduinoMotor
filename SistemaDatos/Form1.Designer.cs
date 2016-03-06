c6db15ccb3c6e4e88ae6c004b4b3e061ef38b195
Luis Iván García Luna <luis.garcialuna@outlook.com>
2016-03-06 08:00:21 -0600

    done

--- SistemaDatos/Form1.Designer.cs
+++ SistemaDatos/Form1.Designer.cs
@@ -28,8 +28,65 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainFrame));
+            this.enlaceBT = new System.IO.Ports.SerialPort(this.components);
+            this.connect = new System.Windows.Forms.Button();
+            this.offline = new System.Windows.Forms.Button();
+            this.send = new System.Windows.Forms.Button();
+            this.TXWINDOW = new System.Windows.Forms.TextBox();
+            this.RXWINDOW = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // connect
+            // 
+            this.connect.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.connect.Location = new System.Drawing.Point(12, 519);
+            this.connect.Name = "connect";
+            this.connect.Size = new System.Drawing.Size(150, 30);
+            this.connect.TabIndex = 0;
+            this.connect.Text = "Conectar";
+            this.connect.UseVisualStyleBackColor = true;
+            this.connect.Click += new System.EventHandler(this.connect_Click);
+            // 
+            // offline
+            // 
+            this.offline.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.offline.Location = new System.Drawing.Point(422, 519);
+            this.offline.Name = "offline";
+            this.offline.Size = new System.Drawing.Size(150, 30);
+            this.offline.TabIndex = 1;
+            this.offline.Text = "Desconectar";
+            this.offline.UseVisualStyleBackColor = true;
+            this.offline.Click += new System.EventHandler(this.offline_Click);
+            // 
+            // send
+            // 
+            this.send.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.send.Location = new System.Drawing.Point(220, 303);
+            this.send.Name = "send";
+            this.send.Size = new System.Drawing.Size(150, 30);
+            this.send.TabIndex = 2;
+            this.send.Text = "Enviar";
+            this.send.UseVisualStyleBackColor = true;
+            this.send.Click += new System.EventHandler(this.send_Click);
+            // 
+            // TXWINDOW
+            // 
+            this.TXWINDOW.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TXWINDOW.Location = new System.Drawing.Point(200, 200);
+            this.TXWINDOW.Name = "TXWINDOW";
+            this.TXWINDOW.Size = new System.Drawing.Size(200, 26);
+            this.TXWINDOW.TabIndex = 3;
+            // 
+            // RXWINDOW
+            // 
+            this.RXWINDOW.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RXWINDOW.Location = new System.Drawing.Point(200, 430);
+            this.RXWINDOW.Name = "RXWINDOW";
+            this.RXWINDOW.Size = new System.Drawing.Size(200, 26);
+            this.RXWINDOW.TabIndex = 4;
+            this.RXWINDOW.TextChanged += new System.EventHandler(this.RXWINDOW_TextChanged);
             // 
             // mainFrame
             // 
@@ -37,7 +94,12 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::SistemaDatos.Properties.Resources.logo;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.ClientSize = new System.Drawing.Size(784, 661);
+            this.ClientSize = new System.Drawing.Size(584, 561);
+            this.Controls.Add(this.RXWINDOW);
+            this.Controls.Add(this.TXWINDOW);
+            this.Controls.Add(this.send);
+            this.Controls.Add(this.offline);
+            this.Controls.Add(this.connect);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
@@ -45,10 +107,18 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Sistema de Adquisición de Datos";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.IO.Ports.SerialPort enlaceBT;
+        private System.Windows.Forms.Button connect;
+        private System.Windows.Forms.Button offline;
+        private System.Windows.Forms.Button send;
+        private System.Windows.Forms.TextBox TXWINDOW;
+        private System.Windows.Forms.TextBox RXWINDOW;
     }
 }
 
--- SistemaDatos/Form1.cs
+++ SistemaDatos/Form1.cs
@@ -16,5 +16,54 @@ namespace SistemaDatos
         {
             InitializeComponent();
         }
+        String RxString;
+
+        private void RXWINDOW_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void offline_Click(object sender, EventArgs e)
+        {
+            if (enlaceBT.IsOpen)
+                enlaceBT.Close();
+            connect.Enabled = true;
+            MessageBox.Show("Disconnected", "Notification", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void connect_Click(object sender, EventArgs e)
+        {
+            enlaceBT.PortName = "COM4";
+            enlaceBT.BaudRate = 9600;
+            enlaceBT.Open();
+
+            if (!enlaceBT.IsOpen) return;
+            connect.Enabled = false;
+
+            MessageBox.Show("Connected", "Notification", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void send_Click(object sender, EventArgs e)
+        {
+            string tx = TXWINDOW.Text;
+            enlaceBT.Write(tx);
+            TXWINDOW.Clear();
+        }
+
+        private void mainFrame_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (enlaceBT.IsOpen) enlaceBT.Close();
+        }
+        private void enlaceBT_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            RxString = enlaceBT.ReadExisting();
+            this.Invoke(new EventHandler(DisplayText));
+
+        }
+        private void DisplayText(object sender, EventArgs e)
+        {
+            RXWINDOW.AppendText(RxString);
+
+        }
     }
 }
