1ed635a0dcda892dc05a786c39411046212a722f
Luis Iván García Luna <luis.garcialuna@outlook.com>
2016-03-06 07:06:58 -0600

    First Commit

---
+++ SistemaDatos/Form1.Designer.cs
@@ -0,0 +1,50 @@
+﻿namespace SistemaDatos
+{
+    partial class mainFrame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // mainFrame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 661);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "mainFrame";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sistema de Adquisición de Datos";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
